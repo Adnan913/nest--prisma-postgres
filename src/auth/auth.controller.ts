64ae8dfc8daa7a03eb354bf6f4c321d5768798aa
Adnan913 <adnansami9134@gmail.com>
2025-02-06 15:18:03 +0500

    added modules and created apis

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,19 @@
-import { Controller, Post, Body } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { User, Bookmark } from "@prisma/client";
-import { AuthDto } from "./dto";
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User, Bookmark } from '@prisma/client';
+import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController{
+export class AuthController {
     constructor(private authService: AuthService) {}
-    
+
     @Post('signup')
-    signup(@Body() dto:AuthDto) {
-        console.log(dto)
-        return this.authService.signup();
+    signup(@Body() dto: AuthDto) {
+        return this.authService.signup(dto);
     }
-    
-    
+
     @Post('signin')
-    signin() {
-        return this.authService.signin();
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signin(dto);
     }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,6 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator"
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class AuthDto{
+export class AuthDto {
     @IsEmail()
     @IsNotEmpty()
     email: string;
